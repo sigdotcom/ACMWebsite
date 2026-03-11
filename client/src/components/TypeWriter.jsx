1b737bdb2e8b9a3292823fd19b7b82b42750d5f1
Elijah <ep4y5@umsystem.edu>
2026-03-10 19:08:10 -0500

    update

--- client/src/components/TypeWriter.jsx
+++ client/src/components/TypeWriter.jsx
@@ -1,1 +1,38 @@
 import { useState, useEffect } from "react";
+
+function TypeWriter({words = [], typeCursorClassName = "", className = "", typeSpeed = 40, deletingSpeed = 50, pauseTime = 4000}){
+    const [wordIndex, setWordIndex] = useState(0);
+    const [displayText, setDisplayText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    useEffect(() =>{
+        const currentWord = words[wordIndex];
+        let timeout;
+    
+        if (!isDeleting && displayText.length < currentWord.length){
+            //typing
+            timeout = setTimeout(() => {
+                setDisplayText(currentWord.slice(0, displayText.length + 1));
+            }, typingSpeed)
+        }
+        else if (!isDeleting && displayText.length === currentWord.length){
+            //pause when finished typing
+            timeout = setTimeout(() => setIsDeleting(true), pauseTime)
+        }
+        else if (isDeleting && displayText.length > 0){
+            //deleting
+            timeout = setTimeout(() => {
+                setDisplayText(currentWord.slice(0, displayText.length - 1));
+            }, deletingSpeed);
+        }
+        else if (isDeleting && displayText.length === 0){
+            //move to next word
+            setIsDeleting(false);
+            setWordIndex((prev) => (prev + 1) % words.length);
+        }
+
+        return () => clearTimeout(timeout); }, [displayText, isDeleting, wordIndex, words, typingSpeed, deletingSpeed, pauseTime]);
+    return <span className={className}>{displayText}<span className={typeCursorClassName}>|</span></span>
+}
+
+export default TypeWriter;
