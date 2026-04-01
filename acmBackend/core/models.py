d343a4e297c775c560d729e8cf1feb3ea91cba94
DanielVeitTheSequel <danielcveit@gmail.com>
2026-04-01 17:47:41 -0500

    maybe merge thing fixed?

--- acmBackend/core/models.py
+++ acmBackend/core/models.py
@@ -5,6 +5,27 @@ from django.db import models
 # comment made by me, Daniel Veit
 # hello there, coal panda x2
 
+<<<<<<< HEAD
 
 #  logo
 #  sig name
+=======
+# sigs
+#  meeting(s)
+#    time
+#    location
+#  description
+#  officers
+#    name
+#    position
+#    image
+#  alumi?
+#  discord
+
+# events
+#   date
+#   location
+#   description
+#   image
+#   
+>>>>>>> 5364c65 (my diagram comments)
