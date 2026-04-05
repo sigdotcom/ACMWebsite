3e6232201e64d6608d02a4ff744a0cd36f3ef0c0
Kush Solanki <solankikush46@gmail.com>
2026-04-05 13:52:13 -0500

    Finished working on the upcoming page and the event popup

--- client/src/pages/general/general.jsx
+++ client/src/pages/general/general.jsx
@@ -1,12 +1,11 @@
 import "./general.css";
 import TypeWriter from "../../components/TypeWriter";
+import UpcomingEvent from "./upcoming_events/upcoming_events";
 
 function General() {
     return (
         <div className="general">
-            <TypeWriter words={["Caffeine"]} />
-            <h1>General Page</h1>
-            <p>This is the general page of the ACM website.</p>
+            <UpcomingEvent />
         </div>
     );
 }
