0e3bc600968faa3255cac83a3006bfd4655f2ac7
Lucas <lmggxf@umsystem.edu>
2026-04-14 18:34:51 -0500

    working on sponsors and animations

--- client/src/pages/general/general.jsx
+++ client/src/pages/general/general.jsx
@@ -2,12 +2,14 @@ import "./general.css";
 import TypeWriter from "../../components/TypeWriter";
 import ACMBoard from "../../components/acm_board/acm_board";
 import UpcomingEvent from "../../components/upcoming_events/upcoming_events";
+import Sponsors from "../../components/sponsors/sponsors";
 
 function General() {
     return (
         <div className="general">
             <UpcomingEvent />
             <ACMBoard />
+            <Sponsors />
         </div>
     );
 }
