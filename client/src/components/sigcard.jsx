85aa1ea2d5b3c5a5d096ca5f57406ac7e8b766d0
Cole Nemkovich <colenemko@gmail.com>
2026-04-14 14:44:13 -0500

    Logos added

--- client/src/components/sigcard.jsx
+++ client/src/components/sigcard.jsx
@@ -4,22 +4,28 @@ function SigCard() {
   return(
     <div className = "card-container">
       <div className = "sig-box hack">
-        <h1>Sigcard1</h1>
+        <img src="src/assets/acm_hack_logo.png" alt="ACM Hack logo" />
+        <h1>ACM Hack</h1>
       </div>
       <div className = "sig-box security">
-        <h1>Sigcard1</h1>
+        <img src="src/assets/ACM_Security_Logo.png" alt="ACM Security logo" />
+        <h1>ACM Security</h1>
       </div>
       <div className = "sig-box data">
-        <h1>Sigcard1</h1>
+        <img src="src/assets/ACM_AIData_Logo.png" alt="ACM AI logo" />
+        <h1>ACM AI</h1>
       </div>
       <div className = "sig-box web">
-        <h1>Sigcard1</h1>
+        <img src="src/assets/acm_web_logo.png" alt="ACM Web logo" />
+        <h1>ACM Web</h1>
       </div>
       <div className = "sig-box game">
-        <h1>Sigcard1</h1>
+        <img src="src/assets/acm_gamedev_logo.png" alt="ACM Game logo" />
+        <h1>ACM Game</h1>
       </div>
-      <div className = "sig-box women">
-        <h1>Sigcard1</h1>
+      <div className = "sig-box comp">
+        <img src="src/assets/acm_comp_logo_trans.png" alt="ACM Comp logo" />
+        <h1>ACM Comp</h1>
       </div>
     
     </div>
