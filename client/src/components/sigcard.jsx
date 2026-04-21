b920a9717d4da6eaa280409ae47c3f58109d072b
Lucas <lmggxf@umsystem.edu>
2026-04-21 13:30:55 -0500

    Changed sig card size, added title, ready for merge

--- client/src/components/sigcard.jsx
+++ client/src/components/sigcard.jsx
@@ -19,7 +19,7 @@ function FlipCard({ className, imgSrc, imgAlt, label }) {
         {/* Back */}
         <div className={`sig-box card-back ${className}`}>
           <h1>{label}</h1>
-          <p>Back content here</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis optio nam, nostrum beatae iure exercitationem ea soluta nobis dicta excepturi, eius, corrupti consequuntur esse ipsum quibusdam voluptatum debitis aut dignissimos?</p>
         </div>
       </div>
     </div>
@@ -28,14 +28,18 @@ function FlipCard({ className, imgSrc, imgAlt, label }) {
 
 function SigCard() {
   return(
-    <div className="card-container">
-      <FlipCard className="hack"     imgSrc="src/assets/acm_hack_logo.png"     imgAlt="ACM Hack logo"     label="ACM Hack"     />
-      <FlipCard className="security" imgSrc="src/assets/ACM_Security_Logo.png" imgAlt="ACM Security logo" label="ACM Security" />
-      <FlipCard className="data"     imgSrc="src/assets/ACM_AIData_Logo.png"   imgAlt="ACM AI logo"       label="ACM AI"       />
-      <FlipCard className="web"      imgSrc="src/assets/acm_web_logo.png"      imgAlt="ACM Web logo"      label="ACM Web"      />
-      <FlipCard className="game"     imgSrc="src/assets/acm_game_logo.png"     imgAlt="ACM Game logo"     label="ACM Game"     />
-      <FlipCard className="comp"     imgSrc="src/assets/acm_comp_logo.png"     imgAlt="ACM Comp logo"     label="ACM Comp"     />
+    <div className="sigcard">
+      <h1>SIGs</h1>
+      <div className="card-container">
+        <FlipCard className="hack"     imgSrc="src/assets/acm_hack_logo.png"     imgAlt="ACM Hack logo"     label="ACM Hack"     />
+        <FlipCard className="security" imgSrc="src/assets/ACM_Security_Logo.png" imgAlt="ACM Security logo" label="ACM Security" />
+        <FlipCard className="data"     imgSrc="src/assets/ACM_AIData_Logo.png"   imgAlt="ACM AI logo"       label="ACM AI"       />
+        <FlipCard className="web"      imgSrc="src/assets/acm_web_logo.png"      imgAlt="ACM Web logo"      label="ACM Web"      />
+        <FlipCard className="game"     imgSrc="src/assets/acm_game_logo.png"     imgAlt="ACM Game logo"     label="ACM Game"     />
+        <FlipCard className="comp"     imgSrc="src/assets/acm_comp_logo.png"     imgAlt="ACM Comp logo"     label="ACM Comp"     />
+      </div>
     </div>
+    
   );
 }
 
