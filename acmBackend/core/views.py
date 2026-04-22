7fad91125829ee611606a295a2c4cd05198c6004
ddandrea039 <ddandrea489@gmail.com>
2026-04-21 19:07:55 -0500

    added classes to the views.py file

--- acmBackend/core/views.py
+++ acmBackend/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from .models import SIG, Event
+from api.serializers import SIGSerializer, EventSerializer
+
+class SIGViewSet(viewsets.ModelViewSet):
+   # Gives endpointts for all the sigs 
+    queryset = SIG.objects.all()
+    serializer_class = SIGSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    #provides endpoints for the api/events/ endpoint
+    serializer_class = EventSerializer
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+        # Captures the 'sig' ID from the URL (e.g., /api/events/?sig=1)
+        sig_id = self.request.query_params.get('sig')
+        
+        if sig_id:
+            # Filters the events table where the related SIG's ID matches
+            queryset = queryset.filter(sig__id=sig_id)
+            
+        return queryset
